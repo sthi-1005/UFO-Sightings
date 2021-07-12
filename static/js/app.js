518fcb509155caf14399fc8bb8b12a383ed683ef
sthi-1005 <79720695+sthi-1005@users.noreply.github.com>
2021-07-11 21:04:59 -0600

    Add files via upload
    line 64-66; changed "row[key] === value" to "row[key].includes(value))"

--- static/js/app.js
+++ static/js/app.js
@@ -62,7 +62,7 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
     Object.entries(filters).forEach(([key, value]) => {
-      filteredData = filteredData.filter(row => row[key] === value);
+      filteredData = filteredData.filter(row => row[key].includes(value));
     });    
   
     // 10. Finally, rebuild the table using the filtered data
